e322b6cf0bb0708f8e06bcf5ba42080c6af2694b
rayahakrosh <rraya879@gmail.com>
2025-08-22 11:21:26 +0300

    Added create

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const { dirname } = require('path');
 const app= express();
-const port = 3000;
+const port = 5000;
 app.use(express.json());
+const tasks=[];
+let nextID=1;
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
+app.get('/t',(req,res)=>{
+    res.status(200). json(tasks)
+})
+app.post('/t',(req,res)=>{
+    let id=nextID++;
+    let text= req.body.txt;
+    let task ={id,text};
+    tasks[id]=task;
+    res.status(201). json({message:"ok"})
+})
+
 app.listen(port,()=>{console.log(`http://localhost:${port}`)});
